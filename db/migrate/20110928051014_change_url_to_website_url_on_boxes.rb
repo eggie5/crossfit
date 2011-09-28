0dd6c66cf659150b32a9967d9a92a504b893cd26
Alex Egg <eggie5@gmail.com>
2011-09-28 14:41:32 -0700

    removing capistrano stuff

--- db/migrate/20110928051014_change_url_to_website_url_on_boxes.rb
+++
@@ -1,9 +0,0 @@
-class ChangeUrlToWebsiteUrlOnBoxes < ActiveRecord::Migration
-  def up
-    change_column :boxes, :url, :website_url
-  end
-
-  def down
-    change_column :boxes, :website_url, :url
-  end
-end
